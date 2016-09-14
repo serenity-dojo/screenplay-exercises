5d1211993e1cfc450557c33fd544897c5c9bdb27
John Smart <john.smart@wakaleo.com>
2016-09-14 12:22:30 +0100

    Starting point

--- src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
+++ src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
@@ -3,8 +3,6 @@ package serenitylabs.tutorials.trains;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.questions.targets.TheTarget;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,16 +12,9 @@ import serenitylabs.tutorials.trains.questions.TheOutboundJourneySummary;
 import serenitylabs.tutorials.trains.tasks.ChosenTo;
 import serenitylabs.tutorials.trains.tasks.EnterHerDetails;
 import serenitylabs.tutorials.trains.tasks.ViewTheAvailableTickets;
-import serenitylabs.tutorials.trains.ui.AssistedTravelPage;
-import serenitylabs.tutorials.trains.ui.TicketTypeForm;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.*;
-import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
-import static net.serenitybdd.screenplay.questions.targets.TheTarget.selectOptionsOf;
-import static net.serenitybdd.screenplay.questions.targets.TheTarget.selectedValueOf;
-import static net.serenitybdd.screenplay.questions.targets.TheTarget.valueOf;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SerenityRunner.class)
 public class WhenPlanningATrip {
@@ -59,43 +50,13 @@ public class WhenPlanningATrip {
     @Test
     public void sensible_default_trip_options_are_proposed() {
         tracy.has(ChosenTo.bookATicket());
-
-        tracy.should(
-                seeThat(the(TicketTypeForm.HEADING), isCurrentlyVisible()),
-                seeThat(the(TicketTypeForm.HEADING), containsText("Buy tickets")),
-
-                seeThat(
-                        TheTarget.attributeNamed("placeholder").forTarget(TicketTypeForm.FROM),
-                        equalTo("From")
-                ),
-
-                seeThat(
-                        TheTarget.cssValueNamed("font").forTarget(TicketTypeForm.HEADING),
-                        containsString("sans-serif")
-                ),
-
-                seeThat(the(TicketTypeForm.SINGLE), isSelected()),
-                seeThat(the(TicketTypeForm.SINGLE), isEnabled()),
-                seeThat(the(TicketTypeForm.RETURN), not(isSelected()))
-        );
+        // TODO
     }
 
     @Test
     public void return_date_should_be_only_enabled_for_return_trips() {
-
         tracy.has(ChosenTo.bookATicket());
-
-        tracy.should(
-                seeThat(the(TicketTypeForm.RETURN_TIME), not(isEnabled()))
-        );
-
-        tracy.attemptsTo(
-                Click.on(TicketTypeForm.RETURN_LABEL)
-        );
-
-        tracy.should(
-                seeThat(the(TicketTypeForm.RETURN_TIME), isEnabled())
-        );
+        // TODO
     }
 
     @Test
@@ -103,11 +64,6 @@ public class WhenPlanningATrip {
         tracy.has(ChosenTo.requestAssistedTravel());
         tracy.attemptsTo(EnterHerDetails.as("Dr", "Tracy", "Traveller"));
 
-        tracy.should(
-                seeThat(valueOf(AssistedTravelPage.FULL_NAME), is("Tracy Traveller")),
-                seeThat(selectedValueOf(AssistedTravelPage.TITLE), is("Dr")),
-                seeThat(selectOptionsOf(AssistedTravelPage.TITLE), hasItems("Mr", "Miss", "Mrs", "Dr", "Other")),
-                seeThat(the(AssistedTravelPage.TITLE), containsSelectOption("Other"))
-        );
+        // TODO
     }
 }
