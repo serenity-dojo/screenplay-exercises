bf0deb6b4f7981966f8ca6da1710d096cbc5952e
John Ferguson Smart <john.smart@wakaleo.com>
2018-04-25 20:42:55 -0400

    Preparing initial state

--- src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
+++ src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
@@ -1,13 +1,26 @@
 package serenitylabs.tutorials.trains;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 
 @RunWith(SerenityRunner.class)
 public class WhenPlanningATrip {
 
+    @Managed
+    WebDriver browser;
+
+    Actor carrie = Actor.named("Carrie");
+
     @Test
     public void planning_a_simple_trip() {
+        carrie.can(BrowseTheWeb.with(browser));
+
+        carrie.attemptsTo(Open.url("https://google.com"));
     }
 }
