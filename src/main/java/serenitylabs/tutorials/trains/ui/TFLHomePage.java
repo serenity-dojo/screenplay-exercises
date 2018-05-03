098db42637730d8d5b19ee46a95c1c92872f9af7
John Ferguson Smart <john.smart@wakaleo.com>
2018-05-03 22:41:30 +0100

    Preparing initial state

--- src/main/java/serenitylabs/tutorials/trains/ui/TFLHomePage.java
+++
@@ -1,9 +0,0 @@
-package serenitylabs.tutorials.trains.ui;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.screenplay.targets.Target;
-import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.By;
-
-@DefaultUrl("https://tfl.gov.uk")
-public class TFLHomePage extends PageObject {}
--- src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
+++ src/test/java/serenitylabs/tutorials/trains/WhenPlanningATrip.java
@@ -1,39 +1,14 @@
 package serenitylabs.tutorials.trains;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.serenitybdd.screenplay.actions.Open;
-import net.serenitybdd.screenplay.questions.page.TheWebPage;
-import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import serenitylabs.tutorials.trains.ui.TFLHomePage;
-
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SerenityRunner.class)
 public class WhenPlanningATrip {
 
-    @Managed
-    WebDriver browser;
-
-    Actor carrie = Actor.named("Carrie");
-
     @Test
     public void the_TFL_page_title_should_be_visible() {
-
-        carrie.can(BrowseTheWeb.with(browser));
-
-        carrie.attemptsTo(Open.browserOn().the(TFLHomePage.class));
-
-        carrie.should(
-                seeThat(
-                        TheWebPage.title(),
-                        containsString("Transport for London")));
     }
-
 
 }
